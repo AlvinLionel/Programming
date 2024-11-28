6ee3d9d3132dde8f8fe987f1ad962286e3e437dc
Lionel-254 <alvinlionel053@gmail.com>
2024-11-28 16:17:33 +0300

    learning

--- c files/Practice.c
+++ c files/Practice.c
@@ -6,7 +6,26 @@
 #include <errno.h>
 int main(){
 
+    FILE* fptr;
+
+    fptr=fopen("text.txt","w");
+    if(fptr==NULL){
+        perror("File open unsuccessful");
+        exit(1);
+    }
+    fputs("C is a fun programming language.\n",fptr);
+    fputs("And,I love using C language",fptr);
     
+    fptr=fopen("text.txt","r");
+    if(fptr==NULL){
+        perror("File open unsuccessful");
+        exit(1);
+    }
+    char content[1000];
+
+    fgets(content,1000,fptr);
+
+    printf("The content contained in the file is:%s",content);
 
     return 0;
 }
