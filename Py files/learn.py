f181c0cac1ff1d86f41a11e75a76973bf81938c7
Lionel-254 <alvinlionel053@gmail.com>
2025-02-17 12:46:44 +0300

    Remove unused files and add new configuration and code files for various languages

--- Py files/learn.py
+++ Py files/learn.py
@@ -1,5 +1,17 @@
-x=[1,2,3]
-y=[4,5,6]
-for x in x:
-    for y in y:
-        print(f"({x} {y})")
+a=12
+b=43.43
+c=32j
+d="string"
+e=True
+f=(1,2,3,4,5)
+g=[1,2,3,4,5]
+h={1,2,3,4,5}
+print(type(a),end=" ")
+print(type(b),end=" ")
+print(type(c),end=" ")
+print(type(d),end=" ")
+print(type(e),end=" ")
+print(type(f),end=" ")
+print(type(g),end=" ")
+print(type(h))
+print(a,b,c,d,e,f,g,h)
--- js files/learn.js
+++ js files/learn.js
@@ -1,3 +1,17 @@
-guest="Alvin Lionel"
-namelength=guest.length;
-console.log(guest);
+var myInterval = setInterval(function () {
+    document.getElementById("time").innerHTML = Date();
+},1000);
+class Fruit {
+    constructor(fruitName, fruitColor) {
+        this.name = fruitName;
+        this.color = fruitColor;
+        this.taste = "sweet";
+    }
+    showDetails() {
+        document.getElementById("demo").innerHTML = "This " + fruit.name + " is " + fruit.color + " and is " + fruit.taste;
+    }
+}
+const fruit = new Fruit("mango", "orange");
+document.addEventListener("DOMContentLoaded",function(){
+    fruit.showDetails();
+})
